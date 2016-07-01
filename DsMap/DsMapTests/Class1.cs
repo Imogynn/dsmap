9ddd6e0f2799d8271ea3dfe2340d0e29ab8729f3
Chris Desmarais <cdesmarais@geotracinternational.com>
2016-07-01 14:02:11 -0600

    And here we go..

---
+++ DsMap/DsMapTests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DsMapTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.That(true);
+        }
+    }
+}
