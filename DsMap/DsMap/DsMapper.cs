90ad45e31a0d9f9ebe9167c555b1c11e372c9bf5
Chris Desmarais <cdesmarais@geotracinternational.com>
2016-07-04 09:55:20 -0600

    Proj changes.

--- DsMap/DsMap/DsMapper.cs
+++ DsMap/DsMap/DsMapper.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Management.Instrumentation;
+﻿using System.Management.Instrumentation;
 using System.Reflection;
 
 namespace DsMap
@@ -11,36 +10,6 @@ namespace DsMap
         public static Cookbook<T> FromRecipe<T>()
         {
             return new Cookbook<T>();
-        }
-    }
-
-    public class Cookbook<T>
-    {
-        public NewClass Create<NewClass>(object ingredients) where NewClass : new()
-        { 
-
-            Type recipeType = typeof(T);
-            Type newClassType = typeof(NewClass);
-            Type oldClassType = ingredients.GetType();
-
-            // need to look for potential other constructors...
-            var result = new NewClass();
-
-            // will want to look at properties so that we can use interfaces for recipes
-            /*foreach (var property in recipeType.GetProperties() )
-            {
-
-            }*/
-
-            foreach (var field in recipeType.GetFields()) // need to consider private fields etc.
-            {
-                var value = oldClassType.GetField(field.Name).GetValue(ingredients);
-                var fieldToSet = newClassType.GetField(field.Name);
-                fieldToSet.SetValue(result, value);
-
-            }
-
-            return result;
         }
     }
 }
